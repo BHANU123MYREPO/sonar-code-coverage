3666851f789acde085574f19f56d78c0a96c1f64
BHANU123MYREPO <prasadbanu800@gmail.com>
2024-12-23 23:10:34 +0530

    Delete frontend/src/App.test.js

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './components/App';
-
-test('renders Hello, MERN with Jest!', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, MERN with Jest!/i);
-  expect(linkElement).toBeInTheDocument();
-});
